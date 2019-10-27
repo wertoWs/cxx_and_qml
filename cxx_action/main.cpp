38c603faaf447342064a4b3844d0351d54c2c972
wertoWs <w-schneider1987@web.de>
2019-10-27 20:40:37 +0900

    1

--- cxx_action/main.cpp
+++ cxx_action/main.cpp
@@ -6,7 +6,7 @@ int main(int argc, char *argv[])
     QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
 
     QGuiApplication app(argc, argv);
-
+//test
     QQmlApplicationEngine engine;
     const QUrl url(QStringLiteral("qrc:/main.qml"));
     QObject::connect(&engine, &QQmlApplicationEngine::objectCreated,
